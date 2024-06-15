a424afdb8ccdf2fdfb6f3ef4f5fb54ce2eb856b6
Rafia-Rather <87219517+Rafia-Rather@users.noreply.github.com>
2024-06-15 09:34:49 +0530

    adds html css n js files for navbar toggle

---
+++ app.js
@@ -0,0 +1,22 @@
+// classList - shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  // one approach
+
+  // if (links.classList.contains("show-links")) {
+  //   links.classList.remove("show-links");
+  // } else {
+  //   links.classList.add("show-links");
+  // }
+
+  // a lot easy another approach in one line
+
+  links.classList.toggle("show-links");
+});
